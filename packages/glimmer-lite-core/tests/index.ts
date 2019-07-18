66dbdc0bc6ff55615cb64d42c55384a0f51bfd04
Tom Dale <tom@tomdale.net>
2019-07-18 15:09:46 -0400

    Add nested component rendering test

--- packages/glimmer-lite-core/tests/index.ts
+++ packages/glimmer-lite-core/tests/index.ts
@@ -26,4 +26,23 @@ module('Smoke test', () => {
     assert.strictEqual(elem.innerHTML, '<h1>Hello world</h1>', 'the template was rendered');
     assert.ok(true, 'rendered');
   });
+
+  test('a component can render multiple nested components', async (assert) => {
+    const Foo = withTemplate(class extends Component {
+    }, `Foo`, () => ({}));
+
+    const Bar = withTemplate(class extends Component {
+    }, `Bar`, () => ({}));
+
+    const OtherComponent = withTemplate(class extends Component {
+    }, `Hello world <Foo /><Bar />`, () => ({ Foo, Bar }));
+
+    const MyComponent = withTemplate(class extends Component {
+    }, `<h1><OtherComponent /></h1>`, () => ({ OtherComponent }));
+
+    const elem = document.getElementById('qunit-fixture')!;
+    await renderComponent(MyComponent, elem);
+
+    assert.strictEqual(elem.innerHTML, '<h1>Hello world FooBar</h1>', 'the template was rendered');
+  });
 });
