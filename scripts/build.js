f8281747a965c525f8ce1076aa75fdc2b0fa7f92
Tom Dale <tom@tomdale.net>
2019-09-11 12:52:21 -0400

    Update build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,21 +1,52 @@
 #!/usr/bin/env node
 
 const path = require('path');
-const fs = require('fs');
-const { execSync } = require('child_process');
-const { sync: globSync } = require('glob');
+const fs = require('fs-extra');
+const { promisify } = require('util');
 
-process.cwd(path.join(__dirname, '..'));
+const exec = promisify(require('child_process').exec);
+const glob = promisify(require('glob'));
 
-try {
-  execSync('node_modules/.bin/tsc -p .');
-} catch (err) {
-  console.log(err.output.toString());
+(async function() {
+  process.cwd(path.join(__dirname, '..'));
+
+  try {
+    await exec('node_modules/.bin/tsc -p .');
+  } catch (err) {
+    if (err.stdout) {
+      console.log(err.stdout.toString());
+    } else {
+      throw err;
+    }
+  }
+
+  const packages = (await glob('dist/packages/{@*/*,!(@*)}'))
+    .map(pkg => pkg.replace('dist/packages/', ''))
+    .map(pkg => ({
+      from: path.join('dist', 'packages', pkg),
+      to: path.join('packages', pkg, 'dist'),
+    }));
+
+  await remove(packages.map(pkg => pkg.to));
+  await move(packages);
+
+  console.log('\n\nDone');
+})();
+
+function remove(paths) {
+  return each(paths, path => {
+    console.log(`Removing ${path}`);
+    return fs.remove(path);
+  });
 }
 
-const packages = globSync('dist/packages/{@*/*,!(@*)}')
-  .map(pkg => pkg.replace('dist/packages/', ''));
+function move(paths) {
+  return each(paths, ({ from, to }) => {
+    console.log(`Moving ${from} -> ${to}`);
+    return fs.rename(from, to);
+  });
+}
 
-packages.forEach(pkg => {
-  fs.renameSync(path.join('dist/packages', pkg), path.join('packages', pkg, 'dist'));
-});
+function each(paths, cb) {
+  return Promise.all(paths.map(cb));
+}
