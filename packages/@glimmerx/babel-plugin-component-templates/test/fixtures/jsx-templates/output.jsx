c4d2dc526ff521a22817af5710709482052bcca0
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-09-11 20:33:35 +0300

    ember-meta-explorer version patch and snapshot update

--- packages/@glimmerx/babel-plugin-component-templates/test/fixtures/jsx-templates/output.jsx
+++ packages/@glimmerx/babel-plugin-component-templates/test/fixtures/jsx-templates/output.jsx
@@ -8,8 +8,6 @@ _setComponentTemplate(Class1Declaration, {
   id: "QZzf93sC",
   block: "{\"symbols\":[\"@lastName\"],\"statements\":[[7,\"div\",true],[9],[1,[24,0,[\"firstName\"]],false],[0,\" \"],[1,[24,1,[]],false],[10]],\"hasEval\":false}",
   meta: {
-    scope: () => ({
-      Class1Declaration
-    })
+    scope: () => ({})
   }
 })
