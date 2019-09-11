beada058d441ba8ee9056cfad7b27fda5555c3ab
Matthew Edwards <medwards@linkedin.com>
2019-09-11 14:09:17 -0700

    Expose option.precompile for precompileOptions
    *Added*
    - An option to the plugin, to pass through precompileOptions to the precompile method.
    - Added a test to see if a AST transform will work.

---
+++ packages/@glimmerx/babel-plugin-component-templates/test/fixtures-options/precompile/ast-transform/code.js
@@ -0,0 +1,3 @@
+const MyComponent = class extends Component {
+  static template = hbs`{{bad}}<h1>Hello world</h1>`;
+};
---
+++ packages/@glimmerx/babel-plugin-component-templates/test/fixtures-options/precompile/ast-transform/output.js
@@ -0,0 +1,12 @@
+
+import { setComponentTemplate as _setComponentTemplate } from "@glimmerx/core";
+
+const MyComponent = _setComponentTemplate(class extends Component {}, {
+  id: "pX6MO7j4",
+  block: "{\"symbols\":[],\"statements\":[[7,\"h1\",true],[9],[0,\"Hello world\"],[10]],\"hasEval\":false}",
+  meta: {
+    scope: () => ({
+      MyComponent
+    })
+  }
+});
