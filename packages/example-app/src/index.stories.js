0d20d0402d6c9e19f4b763aab4ac26d200e15398
jonathantsang <tundralake@gmail.com>
2019-12-20 16:57:21 -0800

    storybook example for example-app

---
+++ packages/example-app/src/index.stories.js
@@ -0,0 +1,6 @@
+import { storiesOf } from '@glimmerx/storybook';
+import Component, { hbs } from '@glimmerx/component';
+import OtherComponent from './OtherComponent'
+
+storiesOf('Example Stories', module)
+  .add('OtherComponent', () => OtherComponent)
