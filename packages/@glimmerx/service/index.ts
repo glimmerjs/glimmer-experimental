503cbd1c8809885821bdd925c8497c9689d82af3
Matthew Edwards <medwards@linkedin.com>
2021-02-11 16:54:53 -0800

    FEAT Improve services API

--- packages/@glimmerx/service/index.ts
+++ packages/@glimmerx/service/index.ts
@@ -1,3 +1,9 @@
 export { service } from './src/decorator';
 
-export default class Service {}
+import { setOwner } from '@glimmerx/core';
+
+export default class Service {
+  constructor(owner) {
+    setOwner(this, owner);
+  }
+}
--- packages/@glimmerx/ssr/tests/render-options-tests.ts
+++ packages/@glimmerx/ssr/tests/render-options-tests.ts
@@ -3,6 +3,7 @@ import HTMLSerializer from '@simple-dom/serializer';
 import voidMap from '@simple-dom/void-map';
 import { SerializableNode } from '@simple-dom/interface';
 import { renderToString, RenderOptions } from '..';
+import Service, { service } from '@glimmerx/service';
 
 QUnit.module('@glimmer/ssr rendering', () => {
   QUnit.test('options.serializer', async (assert) => {
@@ -21,5 +22,66 @@ QUnit.module('@glimmer/ssr rendering', () => {
     const output = await renderToString(MyComponent, options);
 
     assert.equal(output, '<h1>Goodbye World</h1>');
+  });
+
+  QUnit.test('options.services', async (assert) => {
+    const ctx = Object.freeze({
+      locale: 'en_US',
+    });
+
+    class Meta {
+      id;
+
+      constructor(id) {
+        this.id = id;
+      }
+    }
+
+    const meta = new Meta('ABC123');
+
+    class Request extends Service {
+      @service context;
+
+      get locale() {
+        return this.context.locale;
+      }
+    }
+
+    class Locale extends Service {
+      @service request;
+
+      get currentLocale() {
+        return this.request.locale;
+      }
+    }
+
+    class MyComponent extends Component {
+      static template = hbs`<h1>{{this.myLocale}},{{this.metaId}}</h1>`;
+
+      @service locale: Locale;
+      @service meta: Meta;
+
+      get myLocale() {
+        return this.locale.currentLocale;
+      }
+      get metaId() {
+        return this.meta.id;
+      }
+    }
+
+    const services = {
+      context: ctx, // Pojo
+      request: Request, // Constructor
+      locale: Locale, // Constructor,
+      meta: meta,
+    };
+
+    const options: RenderOptions = {
+      services,
+    };
+
+    const output = await renderToString(MyComponent, options);
+
+    assert.equal(output, '<h1>en_US,ABC123</h1>');
   });
 });
