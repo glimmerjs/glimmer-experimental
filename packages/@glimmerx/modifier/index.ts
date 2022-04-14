c89c539ae1c93266cc3a26a4a41c0094f4addf2d
Dan Freeman <dfreeman@salsify.com>
2022-04-14 18:01:56 +0200

    Create an interface for the `on` modifier

--- packages/@glimmerx/modifier/index.ts
+++ packages/@glimmerx/modifier/index.ts
@@ -1,1 +1,13 @@
-export { on, action } from '@glimmer/modifier';
+import { action, on as glimmerOn } from '@glimmer/modifier';
+
+declare const Brand: unique symbol;
+
+// This interface provides an extension point for tools like
+// Glint to augment with template-specific type information.
+export interface OnModifier {
+  [Brand]: 'modifier:on';
+}
+
+const on = glimmerOn as OnModifier;
+
+export { on, action };
